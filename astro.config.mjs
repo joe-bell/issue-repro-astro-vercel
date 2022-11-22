f662af9d639008db8f9b3e602e84a3a93e788ab1
Joe Bell <joe-bell@users.noreply.github.com>
2022-11-22 11:25:24 +1100

    add og example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
+  integrations: [react()],
 });
